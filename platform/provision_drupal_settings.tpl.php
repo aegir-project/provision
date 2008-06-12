11d12e654c270c8860488727083bf5d134da6a8b
Adrian Rossouw <adrian@developmentseed.org>
2008-06-12 19:19:50 +0000

    More work towards restore functionality. Moved the mysql utility functions to provision_mysql.inc, and the help stuff to it's own include too.
    Made more constants to replace the various helper functions, still need to document them.
    
    Replaced all references to db_username with db_user, which is consistent with everywhere else in the system.

--- platform/provision_drupal_settings.tpl.php
+++ platform/provision_drupal_settings.tpl.php
@@ -1,5 +1,5 @@
 
-  $db_url = '<?php print "$site_db_type://$site_db_username:$site_db_passwd@$site_db_host/$site_db_name"; ?>';
+  $db_url = '<?php print "$site_db_type://$site_db_user:$site_db_passwd@$site_db_host/$site_db_name"; ?>';
   $profile = "<?php print $site_profile ?>";
 
   # Additional host wide configuration settings. Useful for safely specifying configuration settings.
