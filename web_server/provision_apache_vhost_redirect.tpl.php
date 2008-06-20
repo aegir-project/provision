df562689c07c44ee3d5716fd9b646634368b9f87
Antoine Beaupré <anarcat@koumbit.org>
2008-06-20 18:48:15 +0000

    fix redirection with non-clean urls sites by using mod_rewrite instead of mod_alias

--- web_server/provision_apache_vhost_redirect.tpl.php
+++ web_server/provision_apache_vhost_redirect.tpl.php
@@ -6,7 +6,9 @@
     
     ServerName <?php print $site_url; ?>
 
-    RedirectMatch ^(.*)$ <?php print $redirect_url ?>
+    RewriteEngine on
+    # the ? at the end is to remove any query string in the original url
+    RewriteRule ^(.*)$ <?php print $redirect_url ?>?
 
     <?php if (is_array($site_aliases)) :
      foreach ($site_aliases as $alias_url) : ?>
