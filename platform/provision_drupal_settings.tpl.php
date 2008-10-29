aa048f473d3c56d7a525b3a1fa7e2dd9322d89ae
Adrian Rossouw <adrian@developmentseed.org>
2008-10-29 13:07:33 +0000

    Stop ini_set warnings on import by adding @ to the calls in the template file (all sites will need to be synched for this to take effect), and closedir after reading all the sites

--- platform/provision_drupal_settings.tpl.php
+++ platform/provision_drupal_settings.tpl.php
@@ -11,17 +11,17 @@
   * settings are used there. Settings defined here should not be
   * duplicated there so as to avoid conflict issues.
   */
-  ini_set('arg_separator.output',     '&amp;');
-  ini_set('magic_quotes_runtime',     0);
-  ini_set('magic_quotes_sybase',      0);
-  ini_set('session.cache_expire',     200000);
-  ini_set('session.cache_limiter',    'none');
-  ini_set('session.cookie_lifetime',  0);
-  ini_set('session.gc_maxlifetime',   200000);
-  ini_set('session.save_handler',     'user');
-  ini_set('session.use_only_cookies', 1);
-  ini_set('session.use_trans_sid',    0);
-  ini_set('url_rewriter.tags',        '');
+  @ini_set('arg_separator.output',     '&amp;');
+  @ini_set('magic_quotes_runtime',     0);
+  @ini_set('magic_quotes_sybase',      0);
+  @ini_set('session.cache_expire',     200000);
+  @ini_set('session.cache_limiter',    'none');
+  @ini_set('session.cookie_lifetime',  0);
+  @ini_set('session.gc_maxlifetime',   200000);
+  @ini_set('session.save_handler',     'user');
+  @ini_set('session.use_only_cookies', 1);
+  @ini_set('session.use_trans_sid',    0);
+  @ini_set('url_rewriter.tags',        '');
 
   global $conf;
   $conf['file_directory_path'] = conf_path() . '/files';
@@ -42,12 +42,7 @@
     // Per RFC 2109, cookie domains must contain at least one dot other than the
     // first. For hosts such as 'localhost', we don't set a cookie domain.
     if (count(explode('.', $domain)) > 2) {
-      ini_set('session.cookie_domain', $domain);
+      @ini_set('session.cookie_domain', $domain);
     }
   }
 
-
-  # Additional host wide configuration settings. Useful for safely specifying configuration settings.
-  if (file_exists('<?php print PROVISION_CONFIG_PATH . '/' ?>includes/global.inc')) {
-    include_once('<?php print PROVISION_CONFIG_PATH . '/' ?>includes/global.inc');
-  }
