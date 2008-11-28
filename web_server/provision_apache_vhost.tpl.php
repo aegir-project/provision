773c7256c5943defa077be24442980a4d0c4daa7
Antoine Beaupré <anarcat@koumbit.org>
2008-11-28 03:36:53 +0000

    new hook: provision_vhost_config, to add stuff to the apache config file for a given site, in the vhost context

--- web_server/provision_apache_vhost.tpl.php
+++ web_server/provision_apache_vhost.tpl.php
@@ -13,6 +13,8 @@
  endforeach;
  endif; ?>
 
+<?php print $extra_config; ?>
+
     # Error handler for Drupal > 4.6.7
     <Directory "<?php print $publish_path; ?>/sites/<?php print trim($site_url, '/'); ?>/files">
       SetHandler This_is_a_Drupal_security_line_do_not_remove
