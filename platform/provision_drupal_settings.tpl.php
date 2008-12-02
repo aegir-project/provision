79263081901c07738828948cec3202caee522a8d
Adrian Rossouw <adrian@developmentseed.org>
2008-12-02 23:00:41 +0000

    Merge all differences between d5 and d6 branches of provision

--- platform/provision_drupal_settings.tpl.php
+++ platform/provision_drupal_settings.tpl.php
@@ -46,3 +46,8 @@
     }
   }
 
+
+  # Additional host wide configuration settings. Useful for safely specifying configuration settings.
+  if (file_exists('<?php print PROVISION_CONFIG_PATH . '/' ?>includes/global.inc')) {
+    include_once('<?php print PROVISION_CONFIG_PATH . '/' ?>includes/global.inc');
+  }
