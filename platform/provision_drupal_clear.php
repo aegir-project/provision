f310a257b8c4e1b20d89315d6cb11617f35b65b2
Adrian Rossouw <adrian@developmentseed.org>
2008-12-11 05:10:57 +0000

    #343428 - move to proc_open, instead of exec, which makes the serialization parsing rock solid. Also, we can now pass an optional array to the calling script, which will allow us to not pass sensitive information over the command line

---
+++ platform/provision_drupal_clear.php
@@ -0,0 +1,36 @@
+<?php
+// $Id$
+/**
+ *  @file
+ *    Rebuild all the caches
+ */
+
+require_once(dirname(__FILE__) . '/../provision.inc');
+if ($argv[1]) {
+  $data = provision_external_init($argv[1]);
+}
+else {
+  provision_set_error(PROVISION_FRAMEWORK_ERROR);
+  provision_log("error", "USAGE: clear.php url\n");
+}
+
+cache_clear_all();
+provision_log('notice', t('Cleared all caches'));
+
+node_types_rebuild();
+provision_log('notice', t('Rebuild node type cache'));
+
+module_rebuild_cache();
+provision_log('notice', t('Rebuild module cache'));
+
+system_theme_data();
+provision_log('notice', t('Rebuild theme cache'));
+
+node_access_rebuild();
+provision_log('notice', t('Rebuild node access cache'));
+
+menu_rebuild();
+provision_log('notice', t('Rebuild menu cache'));
+
+provision_output($argv[1], $data);
+
