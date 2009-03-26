c1c1e975446423e19c682d74df62adbe9f35a249
Antoine Beaupré <anarcat@koumbit.org>
2009-03-26 03:22:14 +0000

    rename the vhost_config hook to apache_vhost_config host and create a apache_dir_config hook to add configuration to the platformwide apache configuration
    include a basic .htacces configuration there, but still allow the .htaccess to override while i do some tests
    
    See: #384972

--- web_server/provision_apache_platform.tpl.php
+++ web_server/provision_apache_platform.tpl.php
@@ -1,6 +1,8 @@
 <Directory <?php print $publish_path; ?>>
+
     Options Indexes FollowSymLinks MultiViews
     AllowOverride All
     Order allow,deny
     Allow from all
+<?php print $extra_config; ?>
 </Directory>
