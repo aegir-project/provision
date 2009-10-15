f983fc4e58a082bfd450e1e417cd69f10825627d
Miguel Jacq <mig@mig5.net>
2009-10-15 10:37:38 +0000

    #605194 - only explode the aliases string if it exists, so a blank ServerAlias isn't generated in the vhost on Disabling of a site. Move the ServerAlias line if it's going to exist, above the RewriteRule, to be consistent with the standard vhost template

--- web_server/provision_apache_vhost_disabled.tpl.php
+++ web_server/provision_apache_vhost_disabled.tpl.php
@@ -6,15 +6,15 @@
     
     ServerName <?php print $site_url; ?>
 
-    RewriteEngine on
-    # the ? at the end is to remove any query string in the original url
-    RewriteRule ^(.*)$ <?php print $redirect_url . '/' . $site_url ?>?
-
     <?php if (is_array($aliases)) :
      foreach ($aliases as $alias) : ?>
        ServerAlias <?php print $alias; ?>
      <?php
        endforeach;
      endif; ?>
+
+    RewriteEngine on
+    # the ? at the end is to remove any query string in the original url
+    RewriteRule ^(.*)$ <?php print $redirect_url . '/' . $site_url ?>?
 
 </VirtualHost>
