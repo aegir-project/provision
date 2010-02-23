c4fa2d3b0e7cda97da80f82588396b10c13cdf76
Antoine Beaupré <anarcat@koumbit.org>
2010-02-22 22:34:43 -0500

    make the server template conform to the installed one

--- web_server/provision_apache_server.tpl.php
+++ web_server/provision_apache_server.tpl.php
@@ -13,4 +13,11 @@ endif;
   LoadModule env_module modules/mod_env.so
 </IfModule>
 
+<IfModule !rewrite_module>
+  LoadModule rewrite_module modules/mod_rewrite.so
+</IfModule>
+
+Include <?php print $config_path ?>/vhost.d/
+Include <?php print $config_path ?>/platform.d/
+
 <?php print $extra_config; ?>
