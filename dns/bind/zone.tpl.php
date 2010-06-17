2baef53843161203b56f8d7fa0ea990d93844663
Antoine Beaupré <anarcat@koumbit.org>
2010-06-17 17:47:40 -0400

    typo

--- dns/bind/zone.tpl.php
+++ dns/bind/zone.tpl.php
@@ -1,5 +1,5 @@
 ; Bind zonefile
 ; File managed by Aegir
-; Changes here may be lost by user configurations, thread carefully
+; Changes here may be lost by user configurations, tread carefully
 $TTL <?php print $ttl; ?>
 
