325cda9a20cb6ad1823b597f707122d3c98a85ab
Adrian Rossouw <adrian@developmentseed.org>
2010-07-14 00:30:31 +0200

    Added key generation code and abstracted the configuration classes a bit more. There are now base http classes that handle the synching to remote servers.

--- http/apache_ssl/server_ssl.tpl.php
+++ http/apache_ssl/server_ssl.tpl.php
@@ -1,9 +1,6 @@
-
-
 <?php foreach ($ip_addresses as $ip) : ?>
   NameVirtualHost <?php print $ip . ":" . $http_ssl_port . "\n"; ?>
 <?php endforeach; ?>
-
 
 <IfModule !ssl_module>
   LoadModule ssl_module modules/mod_ssl.so
