797299a14e1af9852cc89b485e2c4330252dd2b4
Adrian Rossouw <adrian@developmentseed.org>
2010-07-21 01:04:40 +0200

    working provision-zone delete and create zone commands. generates the data store correctly, and uses locking.

--- dns/dnsmasq/server.tpl.php
+++ dns/dnsmasq/server.tpl.php
@@ -1,1 +1,6 @@
-conf-dir=<?php print "$dns_zoned_path"; ?>
+<?php 
+foreach ($records as $key => $name) {
+  printf("conf-file=%s/%s.zone\n", $dns_zoned_path, $name);
+}
+?>
+#conf-dir=<?php print "$dns_zoned_path"; ?>
