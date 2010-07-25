b68f8aa5fa39b2fb0136181a9bc6c9bd29c9f729
Grace <omega8cc@gmail.com>
2010-07-25 03:44:31 +0200

    Allow custom nginx config in conf.d

--- http/nginx/server.tpl.php
+++ http/nginx/server.tpl.php
@@ -97,5 +97,6 @@ server {
 #######################################################
 
 # virtual hosts
+include <?php print $http_confd_path ?>/*;
 include <?php print $http_platformd_path ?>/*;
 include <?php print $http_vhostd_path ?>/*;
