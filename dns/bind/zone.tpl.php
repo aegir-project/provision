7fd69df66bd8791e0eb80c6201c76e2e5ab29637
Adrian Rossouw <adrian@developmentseed.org>
2010-08-04 03:04:29 +0200

    got bind working form install / delete / verify (i think)

--- dns/bind/zone.tpl.php
+++ dns/bind/zone.tpl.php
@@ -1,14 +1,33 @@
 ; Bind zonefile
 ; File managed by Aegir
 ; Changes here may be lost by user configurations, tread carefully
-$TTL <?php print $ttl; ?>
+$TTL <?php print $server->dns_ttl; ?>
 
 <?php
-print("@     IN     SOA  $soa[name] $soa[email] 
-			      $soa[serial] ; serial
-			      $soa[refresh] ; refresh
-			      $soa[retry] ; retry
-			      $soa[expire] ; expire
-			      $soa[minimum] ; minimum
+print("@     IN     SOA  $server->remote_host $dns_email 
+			      $serial ; serial
+			      $server->dns_refresh; refresh
+			      $server->dns_retry ; retry
+			      $server->dns_expire ; expire
+			      $server->dns_negativettl ; minimum
           )\n");
+?>
+
+
+       IN  NS    <?php print $server->remote_host ?>. ; primary DNS 
+       IN  NS     ns2.example.com. ; secondary DNS
+       IN  MX  10 <?php print $server->remote_host ?>. ; external mail provider
+; non server domain hosts
+<?php
+
+foreach ($records['A'] as $ip) {
+  print "   IN  A     {$ip}\n";
+}
+
+foreach ($hosts as $host => $info) {
+  print ";$host\n";
+  foreach ($info['A'] as $ip) {
+    print "{$info['sub']}   IN  A     {$ip}\n";
+  }
+}
 ?>
