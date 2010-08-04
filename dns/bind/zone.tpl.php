2c5fff227a14fcd0aa9d2a7eb9ae66312877a51f
Adrian Rossouw <adrian@developmentseed.org>
2010-08-04 02:35:33 +0200

    more refactoring. strip mining bind implementation.

--- dns/bind/zone.tpl.php
+++ dns/bind/zone.tpl.php
@@ -3,3 +3,12 @@
 ; Changes here may be lost by user configurations, tread carefully
 $TTL <?php print $ttl; ?>
 
+<?php
+print("@     IN     SOA  $soa[name] $soa[email] 
+			      $soa[serial] ; serial
+			      $soa[refresh] ; refresh
+			      $soa[retry] ; retry
+			      $soa[expire] ; expire
+			      $soa[minimum] ; minimum
+          )\n");
+?>
--- dns/dnsmasq/zone.tpl.php
+++ dns/dnsmasq/zone.tpl.php
@@ -1,1 +1,5 @@
-addn-hosts=<?php print "{$dns_hostd_path}/{$name}.hosts"; ?>
+<?php
+  foreach ($hosts as $host => $info) {
+    print "addn-hosts={$dns_hostd_path}/{$host}.hosts\n";
+  }
+?>
