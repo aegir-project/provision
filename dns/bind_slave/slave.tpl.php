7797b8d9b4681560b5682743a2fd6304126fdc24
Adrian Rossouw <adrian@developmentseed.org>
2010-10-26 04:15:24 +0200

    Make slave dns actually work. The master zone data store was being wiped out by the slave server. They use the same data store. Additionally commented out the create_host logic in bind_slave, which is not necessary.
    The code also would not work for anything other than the server context. Which means site dns entries
    were being not only lost, but wiped out.

--- dns/bind_slave/slave.tpl.php
+++ dns/bind_slave/slave.tpl.php
@@ -1,8 +1,5 @@
 <?php 
 foreach ($records as $zone => $master) {
-  if (is_array($master)) {
-    $master = implode(";", $master);
-  }
-  printf('zone "%s" { type slave; file "%s/%s.zone"; masters { %s; }; allow-query { any; }; };' . "\n", $zone, $dns_zoned_path, $zone, $master);
+  printf('zone "%s" { type slave; file "%s/%s.zone"; masters { %s; }; allow-query { any; }; };' . "\n", $zone, $dns_zoned_path, $zone, $master_ip_list);
 }
 ?>
