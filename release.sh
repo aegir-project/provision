b3d448456da2ec948b539893329cfec9969f30b7
anarcat <anarcat@koumbit.org>
2011-02-26 21:15:01 -0500

    switch to drupal.org on the 0.4 branch too

--- release.sh
+++ release.sh
@@ -77,18 +77,18 @@ fi
 git pull
 echo changing makefile to download tarball
 sed -i'.tmp' -e'/^projects\[hostmaster\]\[download\]\[type\]/s/=.*$/ = "get"/' \
-  -e'/^projects\[hostmaster\]\[download\]\[url\]/s#=.*$#= "http://files.aegirproject.org/hostmaster-'$version'.tgz"#' \
+  -e'/^projects\[hostmaster\]\[download\]\[url\]/s#=.*$#= "http://ftp.drupal.org/files/projects/hostmaster-'$version'.tgz"#' \
   -e'/^projects\[hostmaster\]\[download\]\[branch\].*/s/\[branch\] *=.*$/[directory_name] = "hostmaster"/' aegir.make && git add aegir.make && rm aegir.make.tmp
 
 echo changing INSTALL.txt to point to tagged install.sh
-sed -i'.tmp' -e"/http:\/\/git.aegirproject.org\/?p=provision.git;a=blob_plain;f=install.sh.txt;hb=HEAD/s/HEAD/provision-$version/" docs/INSTALL.txt && git add docs/INSTALL.txt && rm docs/INSTALL.txt.tmp
+sed -i'.tmp' -e"/http:\/\/git.drupalcode.org\/project\/provision.git\/blob_plain\/provision-0.4:\/install.sh.txt/s/provision-0.4/provision-$version/" docs/INSTALL.txt && git add docs/INSTALL.txt && rm docs/INSTALL.txt.tmp
 
 echo changing hostmaster-install version
 sed -i'.tmp' -e"s/version *=.*$/version=$version/" provision.info
 git add provision.info && rm provision.info.tmp
 
 echo changing UPGRADE.txt to point to tagged upgrade.sh
-sed -i'.tmp' -e"/http:\/\/git.aegirproject.org\/?p=provision.git;a=blob_plain;f=upgrade.sh.txt;hb=HEAD/s/HEAD/provision-$version/" docs/UPGRADE.txt && git add docs/UPGRADE.txt && rm docs/UPGRADE.txt.tmp
+sed -i'.tmp' -e"/http:\/\/git.drupalcode.org\/project\/provision.git\/blob_plain\/provision-0.4:\/upgrade.sh.txt/s/provision-0.4/provision-$version/" docs/UPGRADE.txt && git add docs/UPGRADE.txt && rm docs/UPGRADE.txt.tmp
 
 echo changing UPGRADE.txt to point to release tags
 sed -i'.tmp' -e"s/export AEGIR_VERSION=HEAD/export AEGIR_VERSION=$version/" docs/UPGRADE.txt
