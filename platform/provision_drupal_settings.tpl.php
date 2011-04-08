4bf9dcac395fdf79ed92c9a7d17e6ca069a61a1e
Barracuda Team <admin@omega8.cc>
2011-04-08 14:09:40 +0200

    API version in settings.php should be displayed also for Nginx based installs.

--- platform/provision_drupal_settings.tpl.php
+++ platform/provision_drupal_settings.tpl.php
@@ -122,7 +122,8 @@
   $conf['file_directory_path'] = 'sites/<?php print $this->uri ?>/files';
   $conf['file_directory_temp'] = 'sites/<?php print $this->uri ?>/files/tmp';
   $conf['clean_url'] = 1;
-  $conf['aegir_api'] = <?php print $this->cloaked ? $this->api_version : 0 ?>;
+  //$conf['aegir_api'] = <?php print $this->cloaked ? $this->api_version : 0 ?>;
+  $conf['aegir_api'] = <?php print $this->api_version : 0 ?>;
 
   <?php if (!$this->site_enabled) : ?>
     $conf['site_offline'] = 1;
