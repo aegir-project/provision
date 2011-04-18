46cacfbc6f8d78cedcc429330aa96aff756d48d2
Steven Jones <steven.jones@computerminds.co.uk>
2011-04-18 22:27:34 +0100

    Fix up the previous commit.

--- http/apache_ssl/vhost_ssl.tpl.php
+++ http/apache_ssl/vhost_ssl.tpl.php
@@ -30,12 +30,11 @@
 
     SSLCertificateKeyFile <?php print $ssl_cert_key; ?>
 
-    <?php
-    if(!empty($ssl_chain_cert)) {
-      print "SSLCertificateChainFile $ssl_chain_cert";
-    }
-    ?>
-<?php 
+  <?php if (!empty($ssl_chain_cert)) : ?>
+    SSLCertificateChainFile <?php print $ssl_chain_cert; ?>
+  <?php endif; ?>
+
+<?php
 if (sizeof($this->aliases)) {
   print "\n ServerAlias " . implode("\n ServerAlias ", $this->aliases) . "\n";
 
