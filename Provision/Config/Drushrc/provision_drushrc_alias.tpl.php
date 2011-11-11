7a064206744dfdaa45ec7e0d745647fddae773d8
Steven Jones <steven.jones@computerminds.co.uk>
2011-11-11 19:56:55 +0000

    Fix code style of provision_drushrc_alias.tpl.php

--- Provision/Config/Drushrc/provision_drushrc_alias.tpl.php
+++ Provision/Config/Drushrc/provision_drushrc_alias.tpl.php
@@ -1,2 +1,7 @@
-<?php print "<?php \n"; ?>
+<?php
+/**
+ * @file
+ * Template file for a drushrc alias file.
+ */
+print "<?php \n"; ?>
 $aliases['<?php print $aliasname; ?>'] = <?php print var_export($options, TRUE); ?>;
