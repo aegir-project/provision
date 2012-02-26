6644df8e8779185f9d6fe831eb7dc5a72425e09a
Miguel Jacq <mig@mig5.net>
2012-02-27 10:16:26 +1100

    Issue #1346712 by mrfelton - Nginx config files required as includes in vhosts are not copied when Nginx SSL feature is enabled.

--- http/Provision/Service/http/nginx/ssl.php
+++ http/Provision/Service/http/nginx/ssl.php
@@ -35,13 +35,13 @@ class Provision_Service_http_nginx_ssl extends Provision_Service_http_ssl {
   }
 
   function verify_server_cmd() {
-     provision_file()->copy(dirname(__FILE__) . '/nginx_advanced_include.conf', $this->server->include_path . '/nginx_advanced_include.conf');
+     provision_file()->copy(dirname(dirname(__FILE__)) . '/nginx_advanced_include.conf', $this->server->include_path . '/nginx_advanced_include.conf');
      $this->sync($this->server->include_path . '/nginx_advanced_include.conf');
-     provision_file()->copy(dirname(__FILE__) . '/nginx_simple_include.conf', $this->server->include_path . '/nginx_simple_include.conf');
+     provision_file()->copy(dirname(dirname(__FILE__)) . '/nginx_simple_include.conf', $this->server->include_path . '/nginx_simple_include.conf');
      $this->sync($this->server->include_path . '/nginx_simple_include.conf');
-     provision_file()->copy(dirname(__FILE__) . '/fastcgi_params.conf', $this->server->include_path . '/fastcgi_params.conf');
+     provision_file()->copy(dirname(dirname(__FILE__)) . '/fastcgi_params.conf', $this->server->include_path . '/fastcgi_params.conf');
      $this->sync($this->server->include_path . '/fastcgi_params.conf');
-     provision_file()->copy(dirname(__FILE__) . '/fastcgi_ssl_params.conf', $this->server->include_path . '/fastcgi_ssl_params.conf');
+     provision_file()->copy(dirname(dirname(__FILE__)) . '/fastcgi_ssl_params.conf', $this->server->include_path . '/fastcgi_ssl_params.conf');
      $this->sync($this->server->include_path . '/fastcgi_ssl_params.conf');
     // Call the parent at the end. it will restart the server when it finishes.
     parent::verify_server_cmd();
