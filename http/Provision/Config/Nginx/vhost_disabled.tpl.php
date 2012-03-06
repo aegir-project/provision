5ce63a77e44ede7e217d68d63fc7bec48bc69a2a
Barracuda Team <admin@omega8.cc>
2012-03-06 13:49:54 +0100

    Fix vhost_disabled.tpl.php

--- http/Provision/Config/Nginx/vhost_disabled.tpl.php
+++ http/Provision/Config/Nginx/vhost_disabled.tpl.php
@@ -2,10 +2,19 @@
 $ip_address = !empty($ip_address) ? $ip_address : '*';
 ?>
 server {
-  listen       <?php print $ip_address . ':' . $http_port; ?>;
+  limit_conn   gulag 18;
+<?php
+if ($ip_address == '*') {
+  print "  listen       {$ip_address}:{$http_port};\n";
+}
+else {
+  foreach ($server->ip_addresses as $ip) {
+    print "  listen       {$ip}:{$http_port};\n";
+  }
+}
+?>
   server_name  <?php print $this->uri . ' ' . implode(' ', $this->aliases); ?>;
   root         /var/www/nginx-default;
   index        index.html index.htm;
-
   ### Dont't reveal Aegir front-end URL here.
 }
