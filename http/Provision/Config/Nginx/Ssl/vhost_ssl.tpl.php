63a41375ba6be7e199907149fcb1601844451c33
Barracuda Team <admin@omega8.cc>
2012-03-06 14:16:04 +0100

    Restore ability to automatic fallback to wildcard listen directive in Nginx and fix spacing.

--- http/Provision/Config/Nginx/Ssl/vhost_ssl.tpl.php
+++ http/Provision/Config/Nginx/Ssl/vhost_ssl.tpl.php
@@ -4,48 +4,49 @@
 <?php if ($this->redirection): ?>
 <?php foreach ($this->aliases as $alias_url): ?>
 server {
-   listen       <?php print "{$ip_address}:{$http_ssl_port}"; ?>;
-   server_name  <?php print $alias_url; ?>;
-   ssl                         on;
-   ssl_certificate             <?php print $ssl_cert; ?>;
-   ssl_certificate_key         <?php print $ssl_cert_key; ?>;
-   ssl_protocols               SSLv3 TLSv1;
-   ssl_ciphers                 HIGH:!ADH:!MD5;
-   ssl_prefer_server_ciphers   on;
-   keepalive_timeout           70;
-   rewrite ^ $scheme://<?php print $this->uri; ?>$request_uri? permanent;
+  limit_conn   gulag 18;
+  listen       <?php print "{$ip_address}:{$http_ssl_port}"; ?>;
+  server_name  <?php print $alias_url; ?>;
+  ssl                        on;
+  ssl_certificate            <?php print $ssl_cert; ?>;
+  ssl_certificate_key        <?php print $ssl_cert_key; ?>;
+  ssl_protocols              SSLv3 TLSv1;
+  ssl_ciphers                HIGH:!ADH:!MD5;
+  ssl_prefer_server_ciphers  on;
+  keepalive_timeout          70;
+  rewrite ^ $scheme://<?php print $this->uri; ?>$request_uri? permanent;
 }
 <?php endforeach; ?>
 <?php endif ?>
 
 server {
-   include      <?php print "{$server->include_path}"; ?>/fastcgi_ssl_params.conf;
-   limit_conn   gulag 18; # like mod_evasive - this allows max 18 simultaneous connections from one IP address
-   listen       <?php print "{$ip_address}:{$http_ssl_port}"; ?>;
-   server_name  <?php print $this->uri; ?><?php if (!$this->redirection && is_array($this->aliases)) : foreach ($this->aliases as $alias_url) : if (trim($alias_url)) : ?> <?php print $alias_url; ?><?php endif; endforeach; endif; ?>;
-   root         <?php print "{$this->root}"; ?>;
-   ssl                         on;
-   ssl_certificate             <?php print $ssl_cert; ?>;
-   ssl_certificate_key         <?php print $ssl_cert_key; ?>;
-   ssl_protocols               SSLv3 TLSv1;
-   ssl_ciphers                 HIGH:!ADH:!MD5;
-   ssl_prefer_server_ciphers   on;
-   keepalive_timeout           70;
-   <?php print $extra_config; ?>
+  include      <?php print "{$server->include_path}"; ?>/fastcgi_ssl_params.conf;
+  limit_conn   gulag 18; # like mod_evasive - this allows max 18 simultaneous connections from one IP address
+  listen       <?php print "{$ip_address}:{$http_ssl_port}"; ?>;
+  server_name  <?php print $this->uri; ?><?php if (!$this->redirection && is_array($this->aliases)) : foreach ($this->aliases as $alias_url) : if (trim($alias_url)) : ?> <?php print $alias_url; ?><?php endif; endforeach; endif; ?>;
+  root         <?php print "{$this->root}"; ?>;
+  ssl                        on;
+  ssl_certificate            <?php print $ssl_cert; ?>;
+  ssl_certificate_key        <?php print $ssl_cert_key; ?>;
+  ssl_protocols              SSLv3 TLSv1;
+  ssl_ciphers                HIGH:!ADH:!MD5;
+  ssl_prefer_server_ciphers  on;
+  keepalive_timeout          70;
+  <?php print $extra_config; ?>
 <?php
 $nginx_has_upload_progress = drush_get_option('nginx_has_upload_progress');
-    if ($nginx_has_upload_progress) {
-      print "   include      " . $server->include_path . "/nginx_advanced_include.conf;\n";
-    }
-    else {
-      print "   include      " . $server->include_path . "/nginx_simple_include.conf;\n";
-    }
+if ($nginx_has_upload_progress) {
+  print "  include      " . $server->include_path . "/nginx_advanced_include.conf;\n";
+}
+else {
+  print "  include      " . $server->include_path . "/nginx_simple_include.conf;\n";
+}
 ?>
 }
 
 <?php endif; ?>
 
 <?php
-   // Generate the standard virtual host too.
-   include(provision_class_directory('Provision_Config_Nginx_Site') . '/vhost.tpl.php');
+  // Generate the standard virtual host too.
+  include(provision_class_directory('Provision_Config_Nginx_Site') . '/vhost.tpl.php');
 ?>
