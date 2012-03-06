9586c1e67ad3c96b296e58b4cf0a6a97bee80a31
Barracuda Team <admin@omega8.cc>
2012-03-06 11:06:07 +0100

    More compact regex for blocked crawlers in Nginx.

--- http/Provision/Config/Nginx/server.tpl.php
+++ http/Provision/Config/Nginx/server.tpl.php
@@ -165,8 +165,8 @@ map $sent_http_x_purge_level $will_expire_in {
 ### Deny crawlers without 403 response.
 ###
 map $http_user_agent $is_crawler {
-  default                                                                                                                              '';
-  ~*HTTrack|MJ12bot|HTMLParser|libwww|PECL|AutomaticSiteMap|ClickSense|ValueClick|SiteBot|BuzzTracker|sistrix|Offline|Screaming|Nutch  is_crawler;
+  default                                                                                                        '';
+  ~*HTTrack|MJ12|HTMLParser|libwww|PECL|Automatic|Click|SiteBot|BuzzTrack|Sistrix|Offline|Screaming|Nutch|Mireo  is_crawler;
 }
 
 ###
