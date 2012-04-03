81e2ab925826ca047c9008e825784f2357cf4f19
Barracuda Team <admin@omega8.cc>
2012-04-03 05:27:14 +0200

    Run this check for local.settings.php only once.

--- Provision/Config/Drupal/Settings.php
+++ Provision/Config/Drupal/Settings.php
@@ -48,15 +48,7 @@ class Provision_Config_Drupal_Settings extends Provision_Config {
     $local_settings = $this->site_path . '/local.settings.php';
     $local_settings_blank = "<?php # local settings.php \n";
     $local_description = 'Drupal local.settings.php file';
-    if (provision_file()->exists($local_settings)->status()) {
-      provision_file()->chgrp($local_settings, $this->group)
-        ->succeed('Changed group ownership of <code>@path</code> to @gid')
-        ->fail('Could not change group ownership of <code>@path</code> to @gid');
-      provision_file()->chmod($local_settings, $this->mode | 0440)
-        ->succeed('Changed permissions of <code>@path</code> to @perm')
-        ->fail('Could not change permissions of <code>@path</code> to @perm');
-    }
-    else {
+    if (!provision_file()->exists($local_settings)->status()) {
       provision_file()->file_put_contents($local_settings, $local_settings_blank)
         ->succeed('Generated blank ' . $local_description)
         ->fail('Could not generate ' . $local_description);
