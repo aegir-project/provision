41bc971e9c1f5c83b22aa7f01aa1aed1e7114af9
Barracuda Team <admin@omega8.cc>
2012-04-05 13:30:06 +0200

    Automatic local.settings.php support fixed.

--- Provision/Config/Drupal/Settings.php
+++ Provision/Config/Drupal/Settings.php
@@ -42,5 +42,29 @@ class Provision_Config_Drupal_Settings extends Provision_Config {
     // then react to this and change any settings we don't want backed up.
     $backup_file = drush_get_option('backup_file');
     $this->backup_in_progress = !empty($backup_file);
+
+    // Create a blank local.settings.php file if not exists.
+    $local_settings = $this->site_path . '/local.settings.php';
+    $local_settings_blank = "<?php # local settings.php \n";
+    $local_description = 'Drupal local.settings.php file';
+    if (!provision_file()->exists($local_settings)->status()) {
+      provision_file()->file_put_contents($local_settings, $local_settings_blank)
+        ->succeed('Generated blank ' . $local_description)
+        ->fail('Could not generate ' . $local_description);
+      provision_file()->chgrp($local_settings, $this->group)
+        ->succeed('Changed group ownership of <code>@path</code> to @gid')
+        ->fail('Could not change group ownership of <code>@path</code> to @gid');
+      provision_file()->chmod($local_settings, $this->mode | 0440)
+        ->succeed('Changed permissions of <code>@path</code> to @perm')
+        ->fail('Could not change permissions of <code>@path</code> to @perm');
+    }
+    else {
+      provision_file()->chgrp($local_settings, $this->group)
+        ->succeed('Changed group ownership of <code>@path</code> to @gid')
+        ->fail('Could not change group ownership of <code>@path</code> to @gid');
+      provision_file()->chmod($local_settings, $this->mode | 0440)
+        ->succeed('Changed permissions of <code>@path</code> to @perm')
+        ->fail('Could not change permissions of <code>@path</code> to @perm');
+    }
   }
 }
