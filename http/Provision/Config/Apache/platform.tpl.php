6eeb5e28ac1f0ecfce5c0f091d20979bb99cd4d4
Antoine Beaupré <anarcat@koumbit.org>
2012-07-26 17:19:51 -0400

    move files and robots.txt rewrite rules to the vhost
    this is to fix #1678876, as we can now access the site URI without
    fooling around SERVER_NAME vs HTTP_HOST

--- http/Provision/Config/Apache/platform.tpl.php
+++ http/Provision/Config/Apache/platform.tpl.php
@@ -3,14 +3,6 @@
     Allow from all
 <?php print $extra_config; ?>
 
-  <IfModule mod_rewrite.c>
-    RewriteEngine on
-    # allow files to be accessed without /sites/fqdn/
-    RewriteRule ^files/(.*)$ /sites/%{HTTP_HOST}/files/$1 [L]
-
-    RewriteCond <?php print $this->root; ?>/sites/%{HTTP_HOST}/files/robots.txt -f
-    RewriteRule ^robots.txt /sites/%{HTTP_HOST}/files/robots.txt [L]
-  </IfModule>
 
 <?php
   if (file_exists("{$this->root}/.htaccess")) {
