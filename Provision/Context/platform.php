52e14dbd5e960ed698a4f440336d9d7957da3493
Steven Jones <steven.jones@computerminds.co.uk>
2012-08-17 14:29:14 +0100

    Issue #1260066 by Steven Jones: Added Allow use of the working copy option when creating a platform.

--- Provision/Context/platform.php
+++ Provision/Context/platform.php
@@ -18,11 +18,13 @@ class Provision_Context_platform extends Provision_Context {
       '--server' => 'platform: drush backend server; default @server_master',
       '--web_server' => 'platform: web server hosting the platform; default @server_master',
       '--makefile' => 'platform: drush makefile to use for building the platform if it doesn\'t already exist',
+      '--make_working_copy' => 'platform: Specifiy TRUE to build the platform with the Drush make --working-copy option.',
     );
   }
 
   function init_platform() {
     $this->setProperty('root');
     $this->setProperty('makefile', '');
+    $this->setProperty('make_working_copy', FALSE);
   }
 }
