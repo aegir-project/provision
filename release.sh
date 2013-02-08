e4d6238c4d0020bf865797cd62239fa955104eec
Antoine Beaupré <anarcat@koumbit.org>
2013-02-08 13:50:26 -0500

    remove unused code in release.sh
    we were constantly changing to git, but we are always using git to
    download the goods in the makefile now for some reason

--- release.sh
+++ release.sh
@@ -77,9 +77,7 @@ echo changing makefile to download tarball
 #sed -i'.tmp' -e'/^projects\[hostmaster\]\[download\]\[type\]/s/=.*$/ = "get"/' \
 #  -e'/^projects\[hostmaster\]\[download\]\[url\]/s#=.*$#= "http://ftp.drupal.org/files/projects/hostmaster-'$major-$version'.tgz"#' \
 #  -e'/^projects\[hostmaster\]\[download\]\[branch\].*/s/\[branch\] *=.*$/[directory_name] = "hostmaster"/' aegir.make && git add aegir.make && rm aegir.make.tmp
-sed -i'.tmp' -e'/^projects\[hostmaster\]\[download\]\[type\]/s/=.*$/= "git"/' \
-  -e'/^projects\[hostmaster\]\[download\]\[url\]/s#=.*$#= "http://git.drupal.org/project/hostmaster.git"#' \
-  -e'/^projects\[hostmaster\]\[download\]\[branch\].*/s/\[branch\] *=.*$/[tag] = "'$major-$version'"/' aegir.make && git add aegir.make && rm aegir.make.tmp
+sed -i'.tmp'-e'/^projects\[hostmaster\]\[download\]\[branch\].*/s/\[branch\] *=.*$/[tag] = "'$major-$version'"/' aegir.make && git add aegir.make && rm aegir.make.tmp
 
 echo changing provision.info version
 sed -i'.tmp' -e"s/version *=.*$/version=$major-$version/" provision.info
@@ -110,7 +108,6 @@ git revert --no-commit $commitid
 git reset --quiet HEAD 'debian/changelog'
 git checkout -- 'debian/changelog'
 git commit
-
 
 if prompt_yes_no "push tags and commits upstream? "; then
     # this makes sure we push the commit *and* the tag
