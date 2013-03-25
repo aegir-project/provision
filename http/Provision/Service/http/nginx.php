c4b26d1295dc944b94d8d1e441b8f162faedc0e7
Barracuda Team <admin@omega8.cc>
2013-03-25 23:13:19 +0100

    Nginx: Basic/Advanced config switch with /etc/nginx/basic_nginx_config.conf control file now works properly on server verify.
    HOW-TO:
    
    $ touch /etc/nginx/basic_nginx_config.conf
    $ su -s /bin/bash - aegir -c "drush @hostmaster hosting-task @server_master verify --force -d"

--- http/Provision/Service/http/nginx.php
+++ http/Provision/Service/http/nginx.php
@@ -14,7 +14,9 @@ class Provision_Service_http_nginx extends Provision_Service_http_public {
     $this->configs['site'][] = 'Provision_Config_Nginx_Site';
     $this->server->setProperty('nginx_is_modern', 0);
     $this->server->setProperty('nginx_has_gzip', 0);
-    $this->server->setProperty('extended_nginx_config', 1);
+    $this->server->setProperty('basic_nginx_config', 0);
+    $this->server->setProperty('extended_nginx_config', 0);
+    $this->server->setProperty('provision_db_cloaking', FALSE);
   }
 
   function save_server() {
@@ -31,28 +33,65 @@ class Provision_Service_http_nginx extends Provision_Service_http_public {
     else {
       return;
     }
-
-    // Use basic nginx configuration if this control file exists.
-    if (provision_file()->exists('/etc/nginx/basic_nginx.conf')->status()) {
-      $this->server->extended_nginx_config = FALSE;
-    }
-
     // Check if some nginx features are supported and save them for later.
     $this->server->shell_exec($path . ' -V');
     $this->server->nginx_is_modern = preg_match("/nginx\/1\.((1\.(8|9|(1[0-9]+)))|((2|3|4|5|6|7|8|9)\.))/", implode('', drush_shell_exec_output()), $match);
     $this->server->nginx_has_gzip = preg_match("/http_gzip_static_module/", implode('', drush_shell_exec_output()), $match);
-    $this->server->provision_db_cloaking = FALSE;
+
+    // Use basic nginx configuration if this control file exists.
+    $basic_nginx_config_file = "/etc/nginx/basic_nginx_config.conf";
+    if (provision_file()->exists($basic_nginx_config_file)->status()) {
+      $this->server->basic_nginx_config = 1;
+      $this->server->extended_nginx_config = 0;
+      drush_log(dt('Basic Nginx Config Active - YES control file found @path.', array('@path' => $basic_nginx_config_file)));
+    }
+    else {
+      $this->server->basic_nginx_config = 0;
+      $this->server->extended_nginx_config = 1;
+      drush_log(dt('Extended Nginx Config Active - NO control file found @path.', array('@path' => $basic_nginx_config_file)));
+    }
   }
 
   function verify_server_cmd() {
-     provision_file()->copy(dirname(__FILE__) . '/nginx_advanced_include.conf', $this->server->include_path . '/nginx_advanced_include.conf');
-     $this->sync($this->server->include_path . '/nginx_advanced_include.conf');
-     provision_file()->copy(dirname(__FILE__) . '/nginx_simple_include.conf', $this->server->include_path . '/nginx_simple_include.conf');
-     $this->sync($this->server->include_path . '/nginx_simple_include.conf');
-     provision_file()->copy(dirname(__FILE__) . '/fastcgi_params.conf', $this->server->include_path . '/fastcgi_params.conf');
-     $this->sync($this->server->include_path . '/fastcgi_params.conf');
-     provision_file()->copy(dirname(__FILE__) . '/fastcgi_ssl_params.conf', $this->server->include_path . '/fastcgi_ssl_params.conf');
-     $this->sync($this->server->include_path . '/fastcgi_ssl_params.conf');
+    // Find nginx executable.
+    if (provision_file()->exists('/usr/local/sbin/nginx')->status()) {
+      $path = "/usr/local/sbin/nginx";
+    }
+    elseif (provision_file()->exists('/usr/sbin/nginx')->status()) {
+      $path = "/usr/sbin/nginx";
+    }
+    elseif (provision_file()->exists('/usr/local/bin/nginx')->status()) {
+      $path = "/usr/local/bin/nginx";
+    }
+    else {
+      return;
+    }
+    // Check if some nginx features are supported and save them for later.
+    $this->server->shell_exec($path . ' -V');
+    $this->server->nginx_is_modern = preg_match("/nginx\/1\.((1\.(8|9|(1[0-9]+)))|((2|3|4|5|6|7|8|9)\.))/", implode('', drush_shell_exec_output()), $match);
+    $this->server->nginx_has_gzip = preg_match("/http_gzip_static_module/", implode('', drush_shell_exec_output()), $match);
+
+    // Use basic nginx configuration if this control file exists.
+    $basic_nginx_config_file = "/etc/nginx/basic_nginx_config.conf";
+    if (provision_file()->exists($basic_nginx_config_file)->status()) {
+      $this->server->basic_nginx_config = 1;
+      $this->server->extended_nginx_config = 0;
+      drush_log(dt('Basic Nginx Config Active - YES control file found @path.', array('@path' => $basic_nginx_config_file)));
+    }
+    else {
+      $this->server->basic_nginx_config = 0;
+      $this->server->extended_nginx_config = 1;
+      drush_log(dt('Extended Nginx Config Active - NO control file found @path.', array('@path' => $basic_nginx_config_file)));
+    }
+
+    provision_file()->copy(dirname(__FILE__) . '/nginx_advanced_include.conf', $this->server->include_path . '/nginx_advanced_include.conf');
+    $this->sync($this->server->include_path . '/nginx_advanced_include.conf');
+    provision_file()->copy(dirname(__FILE__) . '/nginx_simple_include.conf', $this->server->include_path . '/nginx_simple_include.conf');
+    $this->sync($this->server->include_path . '/nginx_simple_include.conf');
+    provision_file()->copy(dirname(__FILE__) . '/fastcgi_params.conf', $this->server->include_path . '/fastcgi_params.conf');
+    $this->sync($this->server->include_path . '/fastcgi_params.conf');
+    provision_file()->copy(dirname(__FILE__) . '/fastcgi_ssl_params.conf', $this->server->include_path . '/fastcgi_ssl_params.conf');
+    $this->sync($this->server->include_path . '/fastcgi_ssl_params.conf');
     // Call the parent at the end. it will restart the server when it finishes.
     parent::verify_server_cmd();
   }
