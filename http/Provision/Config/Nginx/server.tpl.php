a3727640b7a67888963a5ee556f5c47649caca13
Barracuda Team <admin@omega8.cc>
2013-04-05 15:57:29 +0200

    Issue #1961920 by trrroy - nginx: [emerg] invalid number of arguments in "limit_conn_zone" directive in /etc/nginx/conf.d/aegir.conf

--- http/Provision/Config/Nginx/server.tpl.php
+++ http/Provision/Config/Nginx/server.tpl.php
@@ -10,10 +10,10 @@ $nginx_has_gzip = drush_get_option('nginx_has_gzip');
 $extended_nginx_config = drush_get_option('extended_nginx_config');
 
 if ($nginx_is_modern) {
-  print "  limit_conn_zone $binary_remote_addr zone=gulag:10m;\n";
+  print "  limit_conn_zone \$binary_remote_addr zone=gulag:10m;\n";
 }
 else {
-  print "  limit_zone gulag $binary_remote_addr 10m;\n";
+  print "  limit_zone gulag \$binary_remote_addr 10m;\n";
 }
 
 if ($nginx_has_gzip) {
