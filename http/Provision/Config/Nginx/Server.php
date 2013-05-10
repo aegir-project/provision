ca0ed4611af46a4c720e844caa79ff60fae5f1be
Barracuda Team <admin@omega8.cc>
2013-05-11 00:13:55 +0200

    Nginx: Use symlinks for legacy includes instead of duplicate templates to process.

--- http/Provision/Config/Nginx/Server.php
+++ http/Provision/Config/Nginx/Server.php
@@ -18,6 +18,37 @@ class Provision_Config_Nginx_Includes extends Provision_Config_Http_Server {
   public $template = 'vhost_include.tpl.php';
   public $description = 'Nginx web server configuration include file';
 
+  function write() {
+    parent::write();
+
+    if (isset($this->data['application_name'])) {
+      $file = $this->data['application_name'] . '_vhost_common.conf';
+      $legacy_simple_file = $this->data['application_name'] . '_simple_include.conf';
+      $legacy_advanced_file = $this->data['application_name'] . '_advanced_include.conf';
+      // We link both legacy files on the remote server to the right version.
+      $cmda = sprintf('ln -sf %s %s',
+        escapeshellarg($this->data['server']->include_path . '/' . $file),
+        escapeshellarg($this->data['server']->include_path . '/' . $legacy_simple_file)
+      );
+      $cmdb = sprintf('ln -sf %s %s',
+        escapeshellarg($this->data['server']->include_path . '/' . $file),
+        escapeshellarg($this->data['server']->include_path . '/' . $legacy_advanced_file)
+      );
+      if ($this->data['server']->shell_exec($cmda)) {
+        drush_log(dt("Created legacy_simple_file symlink for %file on %server", array(
+          '%file' => $file,
+          '%server' => $this->data['server']->remote_host,
+        )));
+      };
+      if ($this->data['server']->shell_exec($cmdb)) {
+        drush_log(dt("Created legacy_advanced_file symlink for %file on %server", array(
+          '%file' => $file,
+          '%server' => $this->data['server']->remote_host,
+        )));
+      };
+    }
+  }
+
   function filename() {
     if (isset($this->data['application_name'])) {
       $file = $this->data['application_name'] . '_vhost_common.conf';
--- http/Provision/Service/http/nginx.php
+++ http/Provision/Service/http/nginx.php
@@ -84,10 +84,6 @@ class Provision_Service_http_nginx extends Provision_Service_http_public {
       drush_log(dt('Extended Nginx Config Active - NO control file found @path.', array('@path' => $basic_nginx_config_file)));
     }
 
-    provision_file()->copy(dirname(__FILE__) . '/nginx_advanced_include.conf', $this->server->include_path . '/nginx_advanced_include.conf');
-    $this->sync($this->server->include_path . '/nginx_advanced_include.conf');
-    provision_file()->copy(dirname(__FILE__) . '/nginx_simple_include.conf', $this->server->include_path . '/nginx_simple_include.conf');
-    $this->sync($this->server->include_path . '/nginx_simple_include.conf');
     // Call the parent at the end. it will restart the server when it finishes.
     parent::verify_server_cmd();
   }
