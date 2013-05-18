50903bd6739044eced3c41d640385efc1a658afb
Barracuda Team <admin@omega8.cc>
2013-05-18 14:23:48 +0200

    Nginx: Disable access log buffering - it saves some disk I/O but also heavily reduces self-healing and DoS protection effectiveness.

--- http/Provision/Config/Nginx/server.tpl.php
+++ http/Provision/Config/Nginx/server.tpl.php
@@ -126,7 +126,7 @@ if ($nginx_has_upload_progress) {
                          '"$http_user_agent" $request_time "$gzip_ratio"';
 
   client_body_temp_path  /var/lib/nginx/body 1 2;
-  access_log             /var/log/nginx/access.log main buffer=32k;
+  access_log             /var/log/nginx/access.log main;
   error_log              /var/log/nginx/error.log crit;
 
 <?php print $extra_config; ?>
