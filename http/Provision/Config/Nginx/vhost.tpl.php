aegir-project/provision

commit 4bbd969b22c360609748259fedc2a9a2792939c6

explode oneliner so it's more readable, no change

http/Provision/Config/Nginx/vhost.tpl.php
@@ -17,7 +17,15 @@ if ($ssl_redirection || $this->redirection) {
 server {
   limit_conn   gulag 32; # like mod_evasive - this allows max 32 simultaneous connections from one IP address
   listen       *:<?php print $http_port; ?>;
-  server_name  <?php print $this->uri; ?><?php if (!$this->redirection && is_array($this->aliases)) : foreach ($this->aliases as $alias_url) : if (trim($alias_url)) : ?> <?php print $alias_url; ?><?php endif; endforeach; endif; ?>;
+  server_name  <?php
+    print $this->uri;
+    if (!$this->redirection && is_array($this->aliases)) {
+      foreach ($this->aliases as $alias_url) {
+        if (trim($alias_url)) {
+          print $alias_url;
+        }
+      }
+    } ?>;
   root         <?php print "{$this->root}"; ?>;
   <?php print $extra_config; ?>
 <?php

commit 001bc3f7e43ea9321bd9744b8d2835bf44d779cd

properly space out vhost aliases in nginx
fix regression introduced in 4bbd969

http/Provision/Config/Nginx/vhost.tpl.php
@@ -2,6 +2,7 @@
 if ($ssl_redirection || $this->redirection) {
   // Redirect all aliases to the main http url using separate vhosts blocks to avoid if{} in Nginx.
   foreach ($this->aliases as $alias_url) {
+    print "# alias redirection virtual host";
     print "server {\n";
     print "  limit_conn   gulag 32;\n";
     print "  listen       *:{$http_port};\n";
@@ -37,7 +38,7 @@ server {
     if (!$this->redirection && is_array($this->aliases)) {
       foreach ($this->aliases as $alias_url) {
         if (trim($alias_url)) {
-          print $alias_url;
+          print " " . $alias_url;
         }
       }
     } ?>;