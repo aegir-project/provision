3f5d81c6f5a20186e0283015546a85ed63708f94
Antoine Beaupré <anarcat@koumbit.org>
2013-09-25 18:03:56 +0200

    subdirs: forgot to enable mod_rewrite, oddly enough

--- subdirs/Provision/Config/site_location.tpl.php
+++ subdirs/Provision/Config/site_location.tpl.php
@@ -14,11 +14,14 @@ Alias /<?php print $location; ?> <?php print $platform_path; ?>
 
   SetEnv db_port  <?php print urlencode($db_port); ?>
 
+  <IfModule mod_rewrite.c>
+  RewriteEngine on
+
   RewriteCond %{REQUEST_FILENAME} !-f
   RewriteCond %{REQUEST_FILENAME} !-d
   RewriteCond %{REQUEST_URI} !=/favicon.ico
   RewriteRule ^<?php print $platform_path; ?>/(.*)$ /<?php print $location; ?>/index.php?q=$1 [L,QSA]
-
+  </IfModule>
 </Location>
 
 # Error handler for Drupal > 4.6.7
