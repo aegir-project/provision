e31e7c9ea81fcb818387cddf93df171f7fd0f49a
Barracuda Team <admin@omega8.cc>
2013-12-25 03:02:55 +0100

    Remove legacy ini settings.

--- Provision/Config/Drupal/provision_drupal_settings.tpl.php
+++ Provision/Config/Drupal/provision_drupal_settings.tpl.php
@@ -109,11 +109,9 @@ if (isset($_SERVER['db_name'])) {
   @ini_set('arg_separator.output',     '&amp;');
   @ini_set('magic_quotes_runtime',     0);
   @ini_set('magic_quotes_sybase',      0);
-  @ini_set('session.cache_expire',     200000);
   @ini_set('session.cache_limiter',    'none');
-  @ini_set('session.cookie_lifetime',  0);
-  @ini_set('session.gc_maxlifetime',   200000);
   @ini_set('session.save_handler',     'user');
+  @ini_set('session.use_cookies',      1);
   @ini_set('session.use_only_cookies', 1);
   @ini_set('session.use_trans_sid',    0);
   @ini_set('url_rewriter.tags',        '');
