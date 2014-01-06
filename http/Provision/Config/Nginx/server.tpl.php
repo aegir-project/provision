471bb8f67325a6f29d297372a4119255a359e388
Barracuda Team <admin@omega8.cc>
2014-01-06 05:57:02 +0100

    Revert "Revert "Issue #2148335 by mattman - Add Default Localhost Vhost.""
    This reverts commit 93d535b891e95998c4a91603d8da562eee511dfc.

--- http/Provision/Config/Nginx/server.tpl.php
+++ http/Provision/Config/Nginx/server.tpl.php
@@ -247,6 +247,17 @@ else {
   }
 }
 
+server {
+  listen 127.0.0.1:80;
+  server_name 127.0.0.1;
+  location /nginx_status {
+    stub_status on;
+    access_log off;
+    allow 127.0.0.1;
+    deny all;
+  }
+}
+
 #######################################################
 ###  nginx virtual domains
 #######################################################
