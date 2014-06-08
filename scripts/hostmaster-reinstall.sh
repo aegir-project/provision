0d6edd0106b2f0ad83ad44cdc2121c27e4f22538
Christopher Gervais <chris@ergonlogic.com>
2014-06-08 23:46:29 +0200

    Fix re-install script.

--- scripts/hostmaster-reinstall.sh
+++ scripts/hostmaster-reinstall.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-/var/aegir/.drush/provision/hostmaster-purge.sh $1
-/var/aegir/.drush/provision/hostmaster-dev-install.sh $1 $2
+/var/aegir/.drush/provision/scripts/hostmaster-purge.sh $1
+/var/aegir/.drush/provision/scripts/hostmaster-platform.sh
+/var/aegir/.drush/provision/scripts/hostmaster-dev-install.sh $1 $2
