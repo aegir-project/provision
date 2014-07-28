95a1080c3ba2508439ff525c37c7df640116112b
Barracuda Team <admin@omega8.cc>
2014-07-28 14:45:25 +0200

    Nginx: Add DBot to is_crawler list.

--- http/Provision/Config/Nginx/server.tpl.php
+++ http/Provision/Config/Nginx/server.tpl.php
@@ -192,7 +192,7 @@ map $sent_http_x_purge_level $will_expire_in {
 ###
 map $http_user_agent $is_crawler {
   default  '';
-  ~*HTTrack|BrokenLinkCheck|2009042316.*Firefox.*3\.0\.10|MJ12|HTMLParser|PECL|Automatic|SiteBot|BuzzTrack|Sistrix|Offline|Nutch|Mireo|SWEB|Morfeus|GSLFbot|HiScan|Riddler  is_crawler;
+  ~*HTTrack|BrokenLinkCheck|2009042316.*Firefox.*3\.0\.10|MJ12|HTMLParser|PECL|Automatic|SiteBot|BuzzTrack|Sistrix|Offline|Nutch|Mireo|SWEB|Morfeus|GSLFbot|HiScan|Riddler|DBot  is_crawler;
 }
 
 ###
