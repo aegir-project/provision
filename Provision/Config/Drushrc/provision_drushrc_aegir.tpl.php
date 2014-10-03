a3df3facc42a90c3d0473fe8ae77b893b157a597
Herman van Rink <rink@initfour.nl>
2014-10-03 11:17:28 +0200

    Issue #2300537 by helmo | ergonlogic: Added Merge Provision extensions into Hosting modules.

--- Provision/Config/Drushrc/provision_drushrc_aegir.tpl.php
+++ Provision/Config/Drushrc/provision_drushrc_aegir.tpl.php
@@ -3,8 +3,15 @@
  * @file
  * Template file for an Aegir-wide drushrc file.
  */
-print "<?php \n\n\$options['hosting_features'] = ". var_export($hosting_features, TRUE) . ";\n";
+print "<?php \n\n";
 
-print "\$options['exclude'] = ". var_export($drush_exclude, TRUE) . ";\n";
+print "# A list of Aegir features and their enabled status.\n";
+print "\$options['hosting_features'] = ". var_export($hosting_features, TRUE) . ";\n\n";
 
+print "# A list of modules to be excluded because the hosting feature is not enabled.\n";
+print "\$options['exclude'] = ". var_export($drush_exclude, TRUE) . ";\n\n";
+
+print "# A list of paths that drush should include even when working outside\n";
+print "# the context of the hostmaster site.\n";
+print "\$options['include'] = ". var_export($drush_include, TRUE) . ";\n";
 ?>
