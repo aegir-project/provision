f850d30f70e832927b45861e56f8ad1424860659
Antoine Beaupré <anarcat@koumbit.org>
2014-11-20 17:21:12 -0500

    revert previous commit, it's too noisy

--- example/site_data/Provision/Service/site/data.php
+++ example/site_data/Provision/Service/site/data.php
@@ -10,7 +10,6 @@ class Provision_Service_site_data extends Provision_Service {
    * Add the needed properties to the site context.
    */
   static function subscribe_site($context) {
-    drush_log('setting site_data property on {$context->name} context to ' . $context->site_data);
     $context->setProperty('site_data');
   }
 }
