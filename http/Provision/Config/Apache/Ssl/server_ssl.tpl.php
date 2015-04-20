fa64864e24e0ab3ae4a569021ee3ca32cdecfa9d
Christopher Gervais <chris@ergonlogic.com>
2015-04-20 16:48:51 +0000

    Add NameVirtualHost for SSL, regardless of IP addresses.
    This directive is deprecated in Apache 2.4+, and so should be removed at some point.

--- http/Provision/Config/Apache/Ssl/server_ssl.tpl.php
+++ http/Provision/Config/Apache/Ssl/server_ssl.tpl.php
@@ -1,6 +1,4 @@
-<?php foreach ($server->ip_addresses as $ip) : ?>
-  NameVirtualHost <?php print $ip . ":" . $http_ssl_port . "\n"; ?>
-<?php endforeach; ?>
+NameVirtualHost <?php print "*:" . $http_ssl_port . "\n"; ?>
 
 <IfModule !ssl_module>
   LoadModule ssl_module modules/mod_ssl.so
