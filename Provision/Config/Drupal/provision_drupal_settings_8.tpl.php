e7ff93bf5f69f8ca98468d30728c70821b39de74
Barracuda Team <admin@omega8.cc>
2015-09-17 16:29:03 +0200

    Sync D8 and D7 settings.php templates.

--- Provision/Config/Drupal/provision_drupal_settings_8.tpl.php
+++ Provision/Config/Drupal/provision_drupal_settings_8.tpl.php
@@ -119,17 +119,10 @@ if (isset($_SERVER['db_name'])) {
   * settings are used there. Settings defined here should not be
   * duplicated there so as to avoid conflict issues.
   */
-  @ini_set('arg_separator.output',     '&amp;');
-  @ini_set('magic_quotes_runtime',     0);
-  @ini_set('magic_quotes_sybase',      0);
-  @ini_set('session.cache_expire',     200000);
-  @ini_set('session.cache_limiter',    'none');
-  @ini_set('session.cookie_lifetime',  0);
-  @ini_set('session.gc_maxlifetime',   200000);
-  @ini_set('session.save_handler',     'user');
-  @ini_set('session.use_only_cookies', 1);
-  @ini_set('session.use_trans_sid',    0);
-  @ini_set('url_rewriter.tags',        '');
+  @ini_set('session.gc_probability', 1);
+  @ini_set('session.gc_divisor', 100);
+  @ini_set('session.gc_maxlifetime', 200000);
+  @ini_set('session.cookie_lifetime', 2000000);
 
   /**
   * Set the umask so that new directories created by Drupal have the correct permissions
