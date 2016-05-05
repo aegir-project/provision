7b12016dfd1a61790588c290d4ecea127450731a
Herman van Rink <rink@initfour.nl>
2016-05-05 18:55:30 +0200

    Code style

--- Provision/Config/Drushrc/provision_drushrc_site.tpl.php
+++ Provision/Config/Drushrc/provision_drushrc_site.tpl.php
@@ -5,14 +5,15 @@
  */
 print "<?php \n"; ?>
 
-<?php foreach ($option_keys as $key) {
-  print "\n\$options['$key'] = ". var_export(${$key}, TRUE) .';';
-}
+<?php foreach ($option_keys as $key) :
+  print "\n\$options['$key'] = " . var_export(${$key}, TRUE) . ';';
+endforeach;
 ?>
 
-# Aegir additions
-<?php foreach (array('db_type', 'db_port', 'db_host', 'db_user', 'db_passwd', 'db_name') as $key) { ?>
+# Aegir additions.
+<?php foreach (array('db_type', 'db_port', 'db_host', 'db_user', 'db_passwd', 'db_name') as $key): ?>
 $_SERVER['<?php print $key; ?>'] = $options['<?php print $key; ?>'];
-<?php } ?>
-# local non-aegir-generated additions
+<?php endforeach; ?>
+
+# Local non-aegir-generated additions.
 @include_once('<?php print $this->site_path  ?>/local.drushrc.php');
