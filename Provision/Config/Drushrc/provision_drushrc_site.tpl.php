db9d1b9574c4a7071ad9ff4aa84d85d087b2045a
Barracuda Team <admin@omega8.cc>
2016-05-06 17:40:40 +0200

    Sync

--- Provision/Config/Drushrc/provision_drushrc_site.tpl.php
+++ Provision/Config/Drushrc/provision_drushrc_site.tpl.php
@@ -5,12 +5,12 @@
  */
 print "<?php \n"; ?>
 
-<?php foreach ($option_keys as $key) {
-  print "\n\$options['$key'] = ". var_export(${$key}, TRUE) .';';
-}
+<?php foreach ($option_keys as $key) :
+  print "\n\$options['$key'] = " . var_export(${$key}, TRUE) . ';';
+endforeach;
 ?>
 
-# Aegir additions
-<?php foreach (array('db_type', 'db_port', 'db_host', 'db_user', 'db_passwd', 'db_name') as $key) { ?>
+# Aegir additions.
+<?php foreach (array('db_type', 'db_port', 'db_host', 'db_user', 'db_passwd', 'db_name') as $key): ?>
 $_SERVER['<?php print $key; ?>'] = $options['<?php print $key; ?>'];
-<?php } ?>
+<?php endforeach; ?>
