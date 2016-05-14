36ce31f91d4d4068c7603604f5cfe252a158806e
Barracuda Team <admin@omega8.cc>
2016-05-14 02:35:18 +0200

    Map acme-challenge path

--- http/Provision/Config/Nginx/Inc/vhost_include.tpl.php
+++ http/Provision/Config/Nginx/Inc/vhost_include.tpl.php
@@ -79,7 +79,8 @@ if ($is_denied) {
 ###
 ### Support for letsencrypt.org per https://tools.ietf.org/html/rfc5785.
 ###
-location ^~ /.well-known/acme-challenge/ {
+location ^~ /.well-known/acme-challenge {
+  alias /data/disk/EDIT_USER/tools/le/.acme-challenges;
   try_files $uri 404;
 }
 <?php endif; ?>
