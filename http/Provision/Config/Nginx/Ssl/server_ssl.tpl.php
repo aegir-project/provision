44fc200f1fe1bd2b8dd38d6935a242e6d1bd3f01
Barracuda Team <admin@omega8.cc>
2016-05-24 02:51:47 +0200

    Nginx: Sync SSL server template

--- http/Provision/Config/Nginx/Ssl/server_ssl.tpl.php
+++ http/Provision/Config/Nginx/Ssl/server_ssl.tpl.php
@@ -9,12 +9,32 @@ $satellite_mode = drush_get_option('satellite_mode');
 if (!$satellite_mode && $server->satellite_mode) {
   $satellite_mode = $server->satellite_mode;
 }
+
+$nginx_has_http2 = drush_get_option('nginx_has_http2');
+if (!$nginx_has_http2 && $server->nginx_has_http2) {
+  $nginx_has_http2 = $server->nginx_has_http2;
+}
+
+if ($nginx_has_http2) {
+  $ssl_args = "ssl http2";
+}
+else {
+  $ssl_args = "ssl";
+}
+
+if ($satellite_mode == 'boa') {
+  $ssl_listen_ip = "*";
+}
 ?>
 
 server {
+<?php if ($satellite_mode == 'boa'): ?>
+  listen       <?php print "{$ssl_listen_ip}:{$http_ssl_port} {$ssl_args}"; ?>;
+<?php else: ?>
 <?php foreach ($server->ip_addresses as $ip) :?>
-  listen       <?php print $ip . ':' . $http_ssl_port; ?>;
+  listen       <?php print "{$ip}:{$http_ssl_port} {$ssl_args}"; ?>;
 <?php endforeach; ?>
+<?php endif; ?>
   server_name  _;
   location / {
 <?php if ($satellite_mode == 'boa'): ?>
