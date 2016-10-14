cfaab64e66f31f301b17c39829b692f2602cfdc5
Herman van Rink <rink@initfour.nl>
2016-10-14 20:51:09 +0200

    Revert "Make /var/aegir a variable"
    This reverts commit 20a2debd22fe5056505054c20cfdff12ca570bc6.

--- Provision/Config/Drushrc/Aegir.php
+++ Provision/Config/Drushrc/Aegir.php
@@ -24,7 +24,6 @@ class Provision_Config_Drushrc_Aegir extends Provision_Config_Drushrc {
       $enabled_features[$name] = $info['enabled'];
     }
 
-    $this->data['aegir_root'] = DRUPAL_ROOT;
     $this->data['hosting_features'] = $enabled_features;
 
     $this->data['drush_exclude'] = array();
