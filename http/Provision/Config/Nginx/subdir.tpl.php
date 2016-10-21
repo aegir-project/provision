61563a966de7d60cbae0b34588190c6e80790a87
memtkmcc <memtkmcc@92646.no-reply.drupal.org>
2016-10-21 15:24:32 +0200

    Issue #2768869 by memtkmcc: Nginx fastcgi_cache_valid TTL logic is incorrect

--- http/Provision/Config/Nginx/subdir.tpl.php
+++ http/Provision/Config/Nginx/subdir.tpl.php
@@ -1071,11 +1071,10 @@ location ^~ /<?php print $subdir; ?> {
     fastcgi_cache speed;
     fastcgi_cache_methods GET HEAD; ### Nginx default, but added for clarity
     fastcgi_cache_min_uses 1;
-    fastcgi_cache_key "$is_bot$device$host$request_method$key_uri$cache_uid$http_x_forwarded_proto$sent_http_x_local_proto$cookie_respimg$status";
+    fastcgi_cache_key "$scheme$is_bot$device$host$request_method$key_uri$cache_uid$http_x_forwarded_proto$sent_http_x_local_proto$cookie_respimg";
     fastcgi_cache_valid 200 10s;
-    fastcgi_cache_valid 302 1m;
-    fastcgi_cache_valid 301 403 404 5s;
-    fastcgi_cache_valid 500 502 503 504 1s;
+    fastcgi_cache_valid 301 1m;
+    fastcgi_cache_valid 302 403 404 1s;
     fastcgi_cache_lock on;
     fastcgi_ignore_headers Cache-Control Expires;
     fastcgi_pass_header Set-Cookie;
