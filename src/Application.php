d6591303ed6003b258156d6ee493359b844b97d7
Jon Pugh <jon@thinkdrop.net>
2017-09-29 09:25:18 -0400

    Add psy/psysh and "shell" command.

--- src/Application.php
+++ src/Application.php
@@ -3,6 +3,7 @@
 namespace Aegir\Provision;
 
 use Aegir\Provision\Command\SaveCommand;
+use Aegir\Provision\Command\ShellCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Application as BaseApplication;
@@ -30,8 +31,19 @@ class Application extends BaseApplication
    */
   const VERSION = '4.x';
 
+  /**
+   * @var string
+   */
+  const DEFAULT_TIMEZONE = 'America/New_York';
+
   public function __construct()
   {
+
+    // If no timezone is set, set Default.
+    if (empty(ini_get('date.timezone'))) {
+      date_default_timezone_set($this::DEFAULT_TIMEZONE);
+    }
+
     parent::__construct($this::NAME, $this::VERSION);
   }
 
@@ -41,6 +53,7 @@ class Application extends BaseApplication
   protected function getDefaultCommands() {
     $commands = parent::getDefaultCommands();
     $commands[] = new SaveCommand();
+    $commands[] = new ShellCommand();
     return $commands;
   }
 }
