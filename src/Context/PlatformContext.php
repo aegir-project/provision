1145fea209ba4b26acf3a694f1b8b16d64c10b7f
Jon Pugh <jon@thinkdrop.net>
2017-10-02 12:13:10 -0400

    Use correct platform options.

--- src/Context/PlatformContext.php
+++ src/Context/PlatformContext.php
@@ -14,10 +14,11 @@ use Aegir\Provision\Context;
 class PlatformContext extends Context {
   static function option_documentation() {
     $options = array(
-      'remote_host' => 'server: host name; default localhost',
-      'script_user' => 'server: OS user name; default current user',
-      'aegir_root' => 'server: Aegir root; default ' . getenv('HOME'),
-      'master_url' => 'server: Hostmaster URL',
+      'root' => 'platform: path to a Drupal installation',
+      'server' => 'platform: drush backend server; default @server_master',
+      'web_server' => 'platform: web server hosting the platform; default @server_master',
+      'makefile' => 'platform: drush makefile to use for building the platform if it doesn\'t already exist',
+      'make_working_copy' => 'platform: Specifiy TRUE to build the platform with the Drush make --working-copy option.',
     );
     return $options;
   }
