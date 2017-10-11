bd576123bfccc168d3d4f094923c36ef17686d1a
Jon Pugh <jon@thinkdrop.net>
2017-10-11 10:28:28 -0400

    In base Command class, load context and catch exceptions if context not found (unless creating a new context using the "save" command.)

--- src/Command.php
+++ src/Command.php
@@ -64,8 +64,24 @@ abstract class Command extends BaseCommand
         
         // Load active context if a command uses the argument.
         if ($this->input->hasArgument('context_name') && !empty($this->input->getArgument('context_name'))) {
-            $this->context_name = $this->input->getArgument('context_name');
-            $this->context = $this->getApplication()->getContext($this->context_name);
+
+            try {
+                // Load context from context_name argument.
+                $this->context_name = $this->input->getArgument('context_name');
+                $this->context = $this->getApplication()->getContext($this->context_name);
+            }
+            catch (\Exception $e) {
+
+                // If no context with the specified name is found:
+                // if this is "save" command and option for --delete is used, throw exception: context must exist to delete.
+                if ($this->getName() == 'save' && $input->getOption('delete')) {
+                    throw new \Exception("No context named {$this->context_name}. Unable to delete.");
+                }
+                // If this is any other command, context is required.
+                elseif ($this->getName() != 'save') {
+                    throw new \Exception($e->getMessage());
+                }
+            }
         }
     }
 
