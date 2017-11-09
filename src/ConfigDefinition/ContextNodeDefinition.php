347111e8cbc8ed475eedc9924a2e4520a2f38df4
Jon Pugh <jon@thinkdrop.net>
2017-11-09 10:08:52 -0500

    Change to static Application::getContext() method so we don't have to pass the application as an attribute.

--- src/ConfigDefinition/ContextNodeDefinition.php
+++ src/ConfigDefinition/ContextNodeDefinition.php
@@ -2,6 +2,7 @@
 
 namespace Aegir\Provision\ConfigDefinition;
 
+use Aegir\Provision\Application;
 use Symfony\Component\Config\Definition\Builder\ScalarNodeDefinition;
 use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 
@@ -51,10 +52,10 @@ class ContextNodeDefinition extends ScalarNodeDefinition
     {
         // No need to do anything else.
         // If there is no context named $value, getContext() throws an exception for us.
-        $this->parent->getAttribute('application')->getContext($value);
+        Application::getContext($value);
 
         // If context_type is specified, Validate that the desired context is the right type.
-        if ($this->getNode()->getAttribute('context_type') && $this->parent->getAttribute('application')->getContext($value)->type != $this->getNode()->getAttribute('context_type')) {
+        if ($this->getNode()->getAttribute('context_type') && Application::getContext($value)->type != $this->getNode()->getAttribute('context_type')) {
             throw new InvalidConfigurationException(strtr('The context specified for !name must be type !type.', [
                 '!name' => $this->name,
                 '!type' => $this->getNode()->getAttribute('context_type'),
@@ -66,7 +67,7 @@ class ContextNodeDefinition extends ScalarNodeDefinition
             $service = $this->getNode()->getAttribute('service_requirement');
 
             try {
-                $this->parent->getAttribute('application')->getContext($value)->getService($service);
+                Application::getContext($value)->getService($service);
             }
             catch (\Exception $e) {
                 throw new \Exception("Service '$service' does not exist in the specified context '$value'.");
