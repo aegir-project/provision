0e2d263930f11396074783e885fe7fa93749cd62
Jon Pugh <jon@thinkdrop.net>
2018-01-01 11:04:03 -0500

    Reset "start" config.

--- src/Context.php
+++ src/Context.php
@@ -507,6 +507,7 @@ class Context implements BuilderAwareInterface
             $tasks = array_merge($tasks, $service->verify());
 
             foreach ($tasks as $title => $task) {
+                $collection->getConfig()->set('start', '');
                 $collection->getConfig()->set('success', '');
                 $collection->getConfig()->set('failure', '');
     
