9fd1a0daaa2b8e5c117ebf3f7fc234455cf22dea
Jon Pugh <jon@thinkdrop.net>
2018-01-02 12:03:25 -0500

    - Only show starting message on non-logging tasks. - Don't sleep 1 if it's a logging task. - Print exception messages below the task failure row.

--- src/Robo/ProvisionCollection.php
+++ src/Robo/ProvisionCollection.php
@@ -11,6 +11,7 @@ class ProvisionCollection extends Collection {
     
     use ProvisionAwareTrait;
     
+    
     /**
      * Run our tasks, and roll back if necessary.
      *
@@ -43,45 +44,52 @@ class ProvisionCollection extends Collection {
                 }
 
                 // Show starting message.
-                $start_message = !empty($this->getConfig()->get($name . '.start'))? $this->getConfig()->get($name . '.start'): $name;
-                $this->getProvision()->io()->customLite($start_message , '☐');
+                if (strpos($name, 'logging.') !== 0) {
+                    $start_message = !empty($this->getConfig()->get($name . '.start'))? $this->getConfig()->get($name . '.start'): $name;
+                    $this->getProvision()->io()->customLite($start_message , '☐');
+                }
 
                 // If being run interactively, pause momentarily to let user read start message, and replace start message with success or fail.
-                if ($this->getProvision()->getInput()->isInteractive()) {
+                if (strpos($name, 'logging.') !== 0 && $this->getProvision()->getInput()->isInteractive()) {
                     sleep(1);
-
-                    // Erase lines
-                    // @TODO: Detect how many lines were output and erase that many.
-                    $lines = 1;
-                    $this->getProvision()->getOutput()->write(["\x0D"]);
-                    $this->getProvision()->getOutput()->write(["\x1B[2K"]);
-                    if ($lines > 0) {
-                        $this->getProvision()->getOutput()->write(str_repeat("\x1B[1A\x1B[2K", $lines));
-                    }
                 }
 
                 // ROBO
                 $taskList = $taskGroup->getTaskList();
-                $result = $this->runTaskList($name, $taskList, $result);
+                $result = $this->runTaskList($name, $taskList, $result, $print_result);
+                
                 // END ROBO
-
+    
+                if ($this->getProvision()->getInput()->isInteractive()) {
+    
+                    // Erase lines
+                    $lines = 1;
+                    $this->getProvision()->getOutput()->write(["\x0D"]);
+                    $this->getProvision()->getOutput()->write(["\x1B[2K"]);
+                    if ($lines > 0) {
+                        $this->getProvision()->getOutput()->write(
+                          str_repeat("\x1B[1A\x1B[2K", $lines)
+                        );
+                    }
+                }
+        
                 if (!$result->wasSuccessful()) {
-                    
+
                     // Override output with failure() message.
                     if (!empty($this->getConfig()->get($name . '.failure'))) {
-                        $name = $this->getConfig()->get($name . '.failure');
+                        $failure_message = $this->getConfig()->get($name . '.failure');
                     }
                     
-                    // If task failed and there is getMessage, it is the exception message.
-                    if (!empty($result->getMessage())) {
-                        $name = $result->getMessage();
-                    }
                     
                     if ($this->getProvision()->getOutput()->isVerbose()) {
                         $this->getProvision()->io()->errorLite('<options=bold>FAILED </> ' . $name);
                     }
                     else {
-                        $this->getProvision()->io()->errorLite($name);
+                        $this->getProvision()->io()->errorLite($failure_message);
+                        // If task failed and there is getMessage, it is the exception message.
+                        if (!empty($result->getMessage())) {
+                            $this->getProvision()->io()->customLite($result->getMessage(), '   - ');
+                        }
                     }
                     $this->fail();
                     return $result;
@@ -116,10 +124,11 @@ class ProvisionCollection extends Collection {
      *
      * An exact copy of Collection::runTaskList(), because it is private and we need access.
      */
-    private function runTaskList($name, array $taskList, Result $result)
+    private function runTaskList($name, array $taskList, Result $result, $print_result = TRUE)
     {
         try {
             foreach ($taskList as $taskName => $task) {
+                $task->printResult = $this->getProvision()->getOutput()->isVerbose();
                 $taskResult = $this->runSubtask($task);
                 // If the current task returns an error code, then stop
                 // execution and signal a rollback.
@@ -140,7 +149,7 @@ class ProvisionCollection extends Collection {
         } catch (\Exception $e) {
             // Tasks typically should not throw, but if one does, we will
             // convert it into an error and roll back.
-            return Result::fromException($task, $e, $result->getData());
+            return Result::fromException($task, $e, $result->getData(), $print_result);
         }
         return $result;
     }
