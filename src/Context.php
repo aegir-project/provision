6cadda27261802bd9560d40f17b384f70951fdb1
Jon Pugh <jon@thinkdrop.net>
2018-01-22 14:55:17 -0500

    Save $context->properties, not $context->config.

--- src/Context.php
+++ src/Context.php
@@ -450,7 +450,7 @@ class Context implements BuilderAwareInterface
         $dumper = new Dumper();
         
         try {
-            $fs->dumpFile($this->config_path, $dumper->dump($this->config, 10));
+            $fs->dumpFile($this->config_path, $dumper->dump($this->getProperties(), 10));
             return true;
         } catch (IOException $e) {
             return false;
