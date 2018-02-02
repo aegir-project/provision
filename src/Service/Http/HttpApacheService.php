ba5b2cb5f477099af35b2c968a82b96dcb68d0d9
Jon Pugh <jon@thinkdrop.net>
2018-02-02 16:54:59 -0500

    Adding additional TODO About mbstring setting.

--- src/Service/Http/HttpApacheService.php
+++ src/Service/Http/HttpApacheService.php
@@ -178,6 +178,8 @@ $full_error
                 }
 
                 // @TODO: Detect that mod_php is running too.
+                // @TODO: Check that mbstring settings are set to PASS
+    // MacOS default config does not support Drupal without changing this setting.
 
     });
 
