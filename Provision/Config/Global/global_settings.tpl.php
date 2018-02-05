5cfa3335e0a4ad5f1dd17f73ffc9199acff5970d
Jon Pugh <jon@thinkdrop.net>
2018-02-05 13:23:56 -0500

    Issue #20: Remove all 7.x-3.x code

--- Provision/Config/Global/global_settings.tpl.php
+++
@@ -1,2 +0,0 @@
-<?php print '<?php '; ?>
-# global settings.php
--- scripts/hostmaster-platform.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-drush make /var/aegir/.drush/provision/aegir-dev.make --no-gitinfofile --working-copy /var/aegir/hostmaster-7.x-3.x
