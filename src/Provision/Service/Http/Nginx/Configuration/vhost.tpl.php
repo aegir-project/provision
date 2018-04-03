84e40219d8f17ffd76929a6e7db5b91ac3fdb6f6
Jon Pugh <jon@thinkdrop.net>
2018-04-03 10:23:54 -0400

    Don't include fastcgi_param's in the location, otherwise you lose the other fastcgi_params set above.

--- src/Provision/Service/Http/Nginx/Configuration/vhost.tpl.php
+++ src/Provision/Service/Http/Nginx/Configuration/vhost.tpl.php
@@ -113,17 +113,8 @@ if (!$db_port) {
         # Security note: If you're running a version of PHP older than the
         # latest 5.3, you should have "cgi.fix_pathinfo = 0;" in php.ini.
         # See http://serverfault.com/q/627903/94922 for details.
-        include fastcgi_params;
-        # Block httpoxy attacks. See https://httpoxy.org/.
-        fastcgi_param HTTP_PROXY "";
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        fastcgi_param PATH_INFO $fastcgi_path_info;
-        fastcgi_param QUERY_STRING $query_string;
         fastcgi_intercept_errors on;
-        # PHP 5 socket location.
-        #fastcgi_pass unix:/var/run/php5-fpm.sock;
-        # PHP 7 socket location.
-        fastcgi_pass <?php print $php_sock_location ?>;
+        fastcgi_pass <?php print $php_fpm_sock_location ?>;
     }
 
     # Fighting with Styles? This little gem is amazing.
