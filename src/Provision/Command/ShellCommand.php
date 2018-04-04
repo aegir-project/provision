7c06b74d109d5e8c44cadac5be6421e7e565bdb2
Jon Pugh <jon@thinkdrop.net>
2018-04-04 14:12:11 -0400

    Put "shell" command back, but for launching a bash shell with the important environment variables set. Run this command to get into a shell where the `drush` and `drupal` commands work.

--- src/Provision/Command/ShellCommand.php
+++ src/Provision/Command/ShellCommand.php
@@ -32,8 +32,40 @@ class ShellCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $config = new Configuration;
-        $shell = new Shell($config);
-        $shell->run();
+
+        $process = new \Symfony\Component\Process\Process("bash");
+        $process->setTty(TRUE);
+
+        if ($this->context->type == 'site') {
+            $dir = $this->context->getProperty('root');
+            $process->setCommandLine("cd $dir && bash");
+            $this->io->simple("Opening bash shell in " . $dir);
+
+            //@TODO: Allow services to set environment variables for both shell command and virtualhost config.
+            $env = $_SERVER;
+            $env['db_type'] = $this->context->getSubscription('db')->service->getType();
+            $env['db_name'] = $this->context->getSubscription('db')->getProperty('db_name');
+            $env['db_user'] = $this->context->getSubscription('db')->getProperty('db_user');
+            $env['db_passwd'] = $this->context->getSubscription('db')->getProperty('db_password');
+            $env['db_host'] = $this->context->getSubscription('db')->service->provider->getProperty('remote_host');
+            $env['db_port'] = $this->context->getSubscription('db')->service->getCreds()['port'];
+
+            $env['PWD'] = $this->context->getProperty('root');
+
+            // If bin dir is found, add to path
+            if (file_exists($this->context->getProperty('root') . '/bin')) {
+                $env['PATH'] .= ':' . $this->context->getProperty('root') . '/bin';
+            }
+            if (file_exists($this->context->getProperty('root') . '/vendor/bin')) {
+                $env['PATH'] .= ':' . $this->context->getProperty('root') . '/vendor/bin';
+            }
+
+            $process->setEnv($env);
+        }
+        else {
+            $this->io->simple("Opening bash shell in " . getcwd());
+        }
+
+        $process->run();
     }
 }
