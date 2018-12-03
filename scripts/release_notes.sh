0e99c03bb1aa5d84a1eea64653f91a3dde68382a
Herman van Rink <rink@initfour.nl>
2018-12-03 16:01:21 +0100

    Update release_notes.sh to work on a fresh set of clones

--- scripts/release_notes.sh
+++ scripts/release_notes.sh
@@ -9,14 +9,27 @@
 modules="hostmaster provision hosting eldir hosting_civicrm hosting_git hosting_remote_import hosting_site_backup_manager hosting_tasks_extra hosting_logs hosting_https"
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 <previous release tag>"
+  echo "Usage: $0 <previous release tag, e.g. 7.x-3.160>"
+  echo "Best not to use the tags for minor releases, repositories that don't have this tag then fail to generate notes fails "
   exit 1
 fi
 prev_release=$1
 
-cd ..
+CURRENT_BRANCH=7.x-3.x
+
+TEMPDIR=`mktemp --directory`
+
+
+echo "Cloning into temp dir $TEMPDIR..."
+echo
+
+cd $TEMPDIR
 
 for shortname in $modules; do
+
+  # Grab a fresh copy, to avoid projects being on feature branches and having local commits.
+  git clone --quiet --branch $CURRENT_BRANCH git@git.drupal.org:project/$shortname.git >> $TEMPDIR/clone.log
+
   cd $shortname >> /dev/null;
   git pull --quiet
   echo "**Changes to $shortname since $prev_release**"
@@ -35,3 +48,5 @@ for shortname in $modules; do
 done
 
 cd - >> /dev/null;
+
+echo "Fresh clones left in temp dir $TEMPDIR for your conveniance."
